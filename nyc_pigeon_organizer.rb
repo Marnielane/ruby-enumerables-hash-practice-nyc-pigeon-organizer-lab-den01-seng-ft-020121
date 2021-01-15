b567d543b88cc592cef7506f5ea3bd7d927dcf6b
Marnie Robinson <marnielane@gmail.com>
2021-01-15 17:38:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,18 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_hash = data.each_with_object({}) do |(key,value), final_hash|
-    value.each do |colors, pigeons|
-      pigeons.each do |name|
-        if !final_hash[name]
-          final_hash[name] = {}
-        end
-        if !final_hash[name][key] do
-          !final_hash[name][key] = []
-        end
-        final_hash[name][key].push(colors.to_s)
-      end 
-    end
-     binding.pry
+    
+      value.each do |colors, pigeons|
+          pigeons.each do |name|
+              if !final_hash[name]
+              final_hash[name] = {}
+              end
+              if !final_hash[name][key]
+              !final_hash[name][key] = []
+              end
+          final_hash[name][key].push(colors.to_s)
+          end 
+     end
      final_hash
   end
  
