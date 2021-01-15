2d7201f0e2296480358f1e44577dea5950e9af32
Marnie Robinson <marnielane@gmail.com>
2021-01-15 17:39:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
               final_hash[name] = {}
               end
               if !final_hash[name][key]
-              !final_hash[name][key] = []
+              final_hash[name][key] = []
               end
           final_hash[name][key].push(colors.to_s)
           end 
