4e6a7b4e85308a34566fe2b7a05e934eafbaa81c
Marnie Robinson <marnielane@gmail.com>
2021-01-15 16:56:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key,value), new_array|
+   value.each do |inner_key, name|
+   
+   
+   end
    binding.pry 
   
   end
