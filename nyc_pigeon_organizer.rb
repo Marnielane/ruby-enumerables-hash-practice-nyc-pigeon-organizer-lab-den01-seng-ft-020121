1dacbfa18fd236deaf8369a050e450b160d141f1
Marnie Robinson <marnielane@gmail.com>
2021-01-15 17:33:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key,value), final_hash|
+  final_hash = data.each_with_object({}) do |(key,value), final_hash|
     value.each do |colors, pigeons|
       pigeons.each do |name|
         if !final_hash[name]
